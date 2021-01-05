d256faaff9721570aea9e3829b0a3b62eca3dfbc
Chris Kite <chris.kat82@googlemail.com>
2021-01-05 22:22:27 +0000

    second commit

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -1,3 +1,30 @@
 public class WaterBottle {
+    private Integer volume;
 
+    public WaterBottle(Integer volume){
+        this.volume = volume;
+    }
+
+    public Object getVolume() {
+        return this.volume;
+    }
+
+    public int getVolumeAfterDrink() {
+        return this.volume - 10;
+    }
+
+    public int getVolumeAfterEmptied() {
+        return getVolumeAfterDrink() - 90;
+    }
+
+    public int getVolumeAfterFilling() {
+        return getVolumeAfterEmptied() + 100;
+    }
 }
+
+//    Create a water bottle class with a volume property.
+//        The volume should start at 100.
+//        Add a drink function that takes 10 from the volume each time it is called.
+//        Create an empty function that brings the volume down to 0.
+//        Create a fill function that fills the volume back to 100.
+
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,3 +1,42 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
 
+    WaterBottle waterBottle;
+
+    @Before
+    public void setUp(){
+        waterBottle = new WaterBottle(100);
+    }
+
+    @Test
+    public void hasVolume() {
+        assertEquals(100, waterBottle.getVolume());
+    }
+
+    @Test
+    public void takeDrink() {
+        assertEquals(90, waterBottle.getVolumeAfterDrink());
+    }
+
+    @Test
+    public void emptyDrink() {
+        assertEquals(0, waterBottle.getVolumeAfterEmptied());
+    }
+
+    @Test
+    public void fillDrink() {
+        assertEquals(100, waterBottle.getVolumeAfterFilling());
+    }
+
 }
+
+
+//    Create a water bottle class with a volume property.
+//        The volume should start at 100.
+//        Add a drink function that takes 10 from the volume each time it is called.
+//        Create an empty function that brings the volume down to 0.
+//        Create a fill function that fills the volume back to 100.
